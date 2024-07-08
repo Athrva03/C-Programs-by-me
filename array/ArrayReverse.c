281c7fc55fc6b2c691cb9b8c70d0f96221842870
Atharva <ath03d@gmail.com>
2024-07-08 10:21:09 +0530

    commit

--- array/ArrayReverse.c
+++ array/ArrayReverse.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
-int main()
+
+// Function to reverse elements of an array
+void reverse(int arr[], int start, int end)
 {
-    int arr[] = {1, 2, 3, 4, 5, 6};
-    int start = 0;
-    int end = sizeof(arr) / sizeof(arr[0]) - 1;
     while (start < end)
     {
-
         int temp = arr[start];
         arr[start] = arr[end];
         arr[end] = temp;
-
         start++;
         end--;
     }
+}
 
-    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+// Function to rotate the array
+void rotation(int arr[], int size, int d)
+{
+    d = d % size;
+    reverse(arr, 0, size - 1);
+    reverse(arr, 0, d - 1);
+    reverse(arr, d, size - 1);
+}
+
+int main()
+{
+    int d = 3;
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    rotation(arr, size, d);
+
+    for (int i = 0; i < size; i++)
     {
         printf("%d ", arr[i]);
-    } // End for
-} // End main
+    }
+
+    return 0;
+}
